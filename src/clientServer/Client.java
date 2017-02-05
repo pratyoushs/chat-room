d3f5a402025d5d03f81253f772c679b97fb1f30a
Pratyoush Srivastava <pratyoush93@gmail.com>
2017-02-04 22:04:05 -0500

    first commit

---
+++ src/clientServer/Client.java
@@ -0,0 +1,197 @@
+package clientServer;
+
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.*;
+public class Client {
+	private Socket sock;
+	
+	public static void main(String[] args) throws Exception{
+		Client c = new Client();
+		c.startClient();
+	}
+
+	private void startClient() throws Exception {
+		// TODO Auto-generated method stub
+		sock = new Socket("localhost", 444);
+		//ps.println("Hello to server from client");
+		
+		//read from user input using keyboard
+		
+		ExecutorService executor = Executors.newCachedThreadPool();
+		executor.execute(new Send());
+		executor.execute(new Receive());
+			
+	}
+	
+	public class Send implements Runnable{
+		BufferedReader br;
+		PrintStream ps;
+		OutputStream os;
+		public Send(){
+			this.br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				os = sock.getOutputStream();
+				ps = new PrintStream(os);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			String messageSent;
+			while(true){
+				try {
+					messageSent = br.readLine();
+					//System.out.println("Sending...");
+					messageSent.trim();
+					String[] words = messageSent.split(" ");
+					String client;
+					String message = null;
+					int file = 0;
+					
+					if(words[0].equalsIgnoreCase("broadcast")){
+						client = "null";
+						if(words[1].equalsIgnoreCase("file")){
+							message = words[2];
+							file = 1;
+						}
+						else{
+							message = messageSent.substring(words[0].length()+1);
+							System.out.println("Message is " + message);
+						}
+					}
+					else{
+						client = words[1];
+						if(words[2].equalsIgnoreCase("file")){
+							message = words[3];
+							file = 1;
+						}
+						else{
+							message = messageSent.substring(words[0].length() + words[1].length() + 2);
+							System.out.println("Message is " + message);
+						}
+					}
+					//System.out.println(file);
+
+					if(file == 0){
+						sendMessage(words[0], client, message);
+					}
+					else if(file == 1){
+						sendFile(words[0], client, message);						
+					}
+					//ps.println(messageSent);
+				} 
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}	
+		}
+
+		private void sendMessage(String mode, String client, String message) {
+			// TODO Auto-generated method stub
+			System.out.println("sending message\n" + mode + " " + client + " " + message);
+			ps.println(mode.toLowerCase());
+			ps.println(client.toLowerCase());
+			ps.println(message);
+			ps.flush();
+		}
+
+		private void sendFile(String mode, String client, String message) throws IOException {
+			// TODO Auto-generated method stub
+			System.out.println("Sending file...");
+			//ObjectOutputStream out = new ObjectOutputStream(sock.getOutputStream());
+			ps.println(mode.toLowerCase() + " file");
+			ps.println(client.toLowerCase());
+			System.out.println(mode + " " + client + " " + message);
+			File transferFile = new File ("C:\\Users\\Pratyoush\\Desktop\\clientserver\\client1\\"+message);
+			ps.println(transferFile.getName());
+			ps.println(transferFile.length());
+	
+		    byte [] byteArray  = new byte [1024];
+		    //FileInputStream fin = new FileInputStream(transferFile);
+		    int c = 0;
+		    FileInputStream bis = (new FileInputStream(transferFile));
+		 		   		    
+		    while( (c = bis.read(byteArray, 0, byteArray.length)) != -1){
+	    	//ps.print(byteArray);
+	    	os.write(byteArray, 0, c);
+		    }
+		    os.flush();
+		    bis.close();		
+		}
+	}
+	
+	public class Receive implements Runnable{
+		InputStreamReader ir; 
+		BufferedReader br;
+		InputStream is;
+		public Receive(){
+		}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				is = sock.getInputStream();
+				ir = new InputStreamReader(is);
+				br = new BufferedReader(ir);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.println("Ready to receive");
+			String messageRecv;
+			String sender;
+			while(true){
+				
+				try {
+					sender = br.readLine();
+					System.out.print(sender+">   ");
+					messageRecv = br.readLine().trim();
+					if(messageRecv != null){
+						if(messageRecv.equalsIgnoreCase("%file%")){
+							String clientId = br.readLine();
+							String fileName = br.readLine();
+							int fLength = Integer.parseInt(br.readLine());
+							//System.out.println(clientId + fileName + " " + fLength + messageRecv);
+							//System.exit(0);
+							byte[] byteArray = new byte[1024];
+							File outputFile = new File(
+									"C:\\Users\\Pratyoush\\Desktop\\clientserver\\"+clientId+"\\"+fileName);
+							
+							FileOutputStream os = new FileOutputStream(outputFile);
+							if(!outputFile.exists()){
+								outputFile.mkdirs();
+							}
+							int countS = 0;
+							while ( fLength > 0 && (countS = is.read(byteArray, 0, byteArray.length))!= -1) {
+					            //countS = is.read(byteArray,0,byteArray.length);
+					            os.write(byteArray, 0, byteArray.length);
+					            fLength -= countS;
+					            //System.out.println(fLength);
+					        }
+							os.flush();
+							os.close();
+							System.out.println("Sent you a file: " + fileName);							
+						}
+						else{
+							System.out.println(messageRecv);
+						}
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}		
+			
+		}
+		
+	}
+	
+}
---
+++ src/clientServer/Server.java
@@ -0,0 +1,310 @@
+package clientServer;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server{
+	private static final int PORT = 444;
+	private static HashMap<Socket, String> socketMap = new HashMap<Socket, String>();
+	private static int clientCount = 0;
+	static ArrayList<Socket> allSockets = new ArrayList<Socket>();
+
+	public static void main(String[] args) throws Exception{
+		ServerSocket servsock = new ServerSocket(PORT);
+		if(servsock != null){
+			System.out.println("server started");
+		}
+		ExecutorService executor = Executors.newCachedThreadPool();
+		while(true){
+			Socket sock = servsock.accept();
+
+			//this blocks the server. make it part of clienthandler
+			//String option = printMenu(sock);
+			clientCount++;
+			socketMap.put(sock, "client"+clientCount);
+			executor.execute(new ClientHandler(sock));
+		}
+	}
+
+	public static String printMenu(Socket sock) throws IOException{
+		PrintStream ps = new PrintStream(sock.getOutputStream());
+		ps.println("Enter input mode");
+		ps.println("1. Broadcast");
+		ps.println("2. Unicast");
+		ps.println("3. Block a client");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		while(true){
+			String option = br.readLine();
+			if(option != null){
+				return option;
+			}
+		}
+	}
+
+	private static class ClientHandler implements Runnable{
+		Socket sock;
+		BufferedReader br;
+		String message;
+		String mode;
+		String client;
+		public ClientHandler(Socket sock){
+			this.sock = sock;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try{
+				System.out.println("New client added with port number " + sock.getPort());
+
+				while(true){
+					br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+					mode = br.readLine();
+					client = br.readLine();
+					System.out.println(mode + "\n" + client);
+					//bmessage = br.readLine();
+					switch(mode){
+					case "broadcast" : broadCast();
+					break;
+					case "unicast" :
+						uniCast(client);
+						break;
+					case "blockcast" :
+						blockClient(client);
+						break;
+					case "broadcast file" : 
+						broadCastFile();
+						break;
+					case "unicast file" :
+						try {
+							uniCastFile(client);
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						break;
+					case "blockcast file" :
+						try {
+							blockClientFile(client);
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			catch(IOException ignore){};
+		}
+
+		private void blockClientFile(String client) throws Exception {
+			// TODO Auto-generated method stub
+			InputStream is = sock.getInputStream();
+			String fileName = br.readLine();
+			int fileLength = Integer.parseInt(br.readLine());
+			byte[] byteArray = new byte[1024];
+			int countS;
+			int fLength = fileLength;
+			File transferFile = new File("newservercopy\\"+fileName);
+			FileOutputStream fos = new FileOutputStream(transferFile);
+			while( fLength > 0 && (countS = is.read(byteArray, 0, byteArray.length)) != -1){
+				fos.write(byteArray, 0, byteArray.length);
+				fLength -= countS;
+			}
+			fos.flush();
+			fos.close();
+			System.out.println("Server received");
+			for(Socket s: socketMap.keySet()){
+				if(s.getPort() != sock.getPort() && !socketMap.get(s).equals(client)){
+					OutputStream os = s.getOutputStream();
+					PrintStream ps = new PrintStream(os);
+					FileInputStream fis = new FileInputStream(transferFile);
+					byteArray = new byte[1024];
+					countS = 0;
+					fLength = fileLength;
+					
+					ps.println(socketMap.get(sock));
+					ps.println("%File%");
+					ps.println(socketMap.get(s));
+					ps.println(fileName);
+					ps.println(fileLength);
+
+					while((countS = fis.read(byteArray, 0, byteArray.length)) != -1){
+						os.write(byteArray,0,byteArray.length);
+					}
+					os.flush();
+					fis.close();
+					System.out.println("File sent to " + socketMap.get(s));
+				}
+			}
+			
+		}
+
+		private void uniCastFile(String client) throws Exception {
+			// TODO Auto-generated method stub
+			InputStream is = sock.getInputStream();
+			String fileName = br.readLine();
+			int fileLength = Integer.parseInt(br.readLine());
+			byte[] byteArray = new byte[1024];
+			int countS;
+			int fLength = fileLength;
+			File transferFile = new File("newservercopy\\"+fileName);
+			FileOutputStream fos = new FileOutputStream(transferFile);
+			while( fLength > 0 && (countS = is.read(byteArray, 0, byteArray.length)) != -1){
+				fos.write(byteArray, 0, byteArray.length);
+				fLength -= countS;
+			}
+			fos.flush();
+			fos.close();
+			System.out.println("Server received");
+			for(Socket s: socketMap.keySet()){
+				if(s.getPort() != sock.getPort() && socketMap.get(s).equals(client)){
+					OutputStream os = s.getOutputStream();
+					PrintStream ps = new PrintStream(os);
+					FileInputStream fis = new FileInputStream(transferFile);
+					byteArray = new byte[1024];
+					countS = 0;
+					fLength = fileLength;
+					
+					ps.println(socketMap.get(sock));
+					ps.println("%File%");
+					ps.println(socketMap.get(s));
+					ps.println(fileName);
+					ps.println(fileLength);
+
+					while((countS = fis.read(byteArray, 0, byteArray.length)) != -1){
+						os.write(byteArray,0,byteArray.length);
+					}
+					os.flush();
+					fis.close();
+					System.out.println("File sent to " + socketMap.get(s));
+				}
+			}
+			
+		}
+
+		private void broadCastFile() throws IOException {
+			// TODO Auto-generated method stub
+			InputStream is = sock.getInputStream();
+			String fileName = br.readLine();
+			int fileLength = Integer.parseInt(br.readLine());
+			byte[] byteArray = new byte[1024];
+			int countS;
+			int fLength = fileLength;
+			File transferFile = new File("newservercopy\\"+fileName);
+			FileOutputStream fos = new FileOutputStream(transferFile);
+			while( fLength > 0 && (countS = is.read(byteArray, 0, byteArray.length)) != -1){
+				fos.write(byteArray, 0, byteArray.length);
+				fLength -= countS;
+			}
+			fos.flush();
+			fos.close();
+			System.out.println("Server received");
+			for(Socket s: socketMap.keySet()){
+				if(s.getPort() != sock.getPort()){
+					OutputStream os = s.getOutputStream();
+					PrintStream ps = new PrintStream(os);
+					FileInputStream fis = new FileInputStream(transferFile);
+					byteArray = new byte[1024];
+					countS = 0;
+					fLength = fileLength;
+					
+					ps.println(socketMap.get(sock));
+					ps.println("%File%");
+					ps.println(socketMap.get(s));
+					ps.println(fileName);
+					ps.println(fileLength);
+
+					while((countS = fis.read(byteArray, 0, byteArray.length)) != -1){
+						os.write(byteArray,0,byteArray.length);
+					}
+					os.flush();
+					fis.close();
+					System.out.println("File sent to " + socketMap.get(s));
+				}
+			}
+		}
+
+		private void blockClient(String client) throws IOException {
+			// TODO Auto-generated method stub
+			message = br.readLine();
+			for(Map.Entry e: socketMap.entrySet())
+			{
+				Socket s = (Socket)e.getKey();
+				if(s.getPort()!= sock.getPort() && !((String)e.getValue()).equals(client))
+				{
+					PrintStream ps = new PrintStream(s.getOutputStream());
+					ps.println(socketMap.get(sock));
+					ps.println(message);
+					System.out.println("message sent");
+				}
+			}
+
+		}
+
+		private void uniCast(String client) throws IOException {
+			// TODO Auto-generated method stub
+			message = br.readLine();
+			{
+
+				for(Map.Entry e: socketMap.entrySet())
+				{  Socket s = (Socket)e.getKey();
+				if(s.getPort()!=sock.getPort() && ((String)e.getValue()).equals(client))
+				{
+					PrintStream ps = new PrintStream(s.getOutputStream());
+					ps.println(socketMap.get(sock));
+					ps.println(message);
+					System.out.println("message sent");
+				}
+				}
+
+			}
+
+
+		}
+
+		private void broadCast() throws IOException {
+			// TODO Auto-generated method stub
+			message = br.readLine();
+			System.out.println(message);
+			for(Socket s: socketMap.keySet()){
+				if(message != null && s.getPort()!= sock.getPort()){
+					System.out.println(message + " " + s.getPort() + " " + sock.getPort());
+
+					PrintStream ps = new PrintStream(s.getOutputStream());
+					ps.println(socketMap.get(sock));
+					ps.println(message);
+					ps.flush();
+					System.out.println("message sent");
+				}
+				else{
+					//sock.close();
+				}
+
+			}
+		}
+	}
+
+}
