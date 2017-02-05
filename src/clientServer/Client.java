76fef312e22f8da29a0f45664f181a4bf22f5425
Pratyoush Srivastava <pratyoush93@gmail.com>
2017-02-04 22:25:08 -0500

    Improves message format on client's side and error catching.

--- src/clientServer/Client.java
+++ src/clientServer/Client.java
@@ -15,14 +15,10 @@ public class Client {
 	private void startClient() throws Exception {
 		// TODO Auto-generated method stub
 		sock = new Socket("localhost", 444);
-		//ps.println("Hello to server from client");
-		
-		//read from user input using keyboard
-		
+				
 		ExecutorService executor = Executors.newCachedThreadPool();
 		executor.execute(new Send());
-		executor.execute(new Receive());
-			
+		executor.execute(new Receive());		
 	}
 	
 	public class Send implements Runnable{
@@ -43,12 +39,17 @@ public class Client {
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
+			System.out.println("You are connected!");
 			String messageSent;
 			while(true){
 				try {
 					messageSent = br.readLine();
 					//System.out.println("Sending...");
 					messageSent.trim();
+					if(messageSent.equalsIgnoreCase("bye")){
+						System.out.println("Exiting...");
+						System.exit(1);
+					}
 					String[] words = messageSent.split(" ");
 					String client;
 					String message = null;
@@ -62,10 +63,10 @@ public class Client {
 						}
 						else{
 							message = messageSent.substring(words[0].length()+1);
-							System.out.println("Message is " + message);
+							//System.out.println("Message is " + message);
 						}
 					}
-					else{
+					else if(words[0].equalsIgnoreCase("blockcast") || words[0].equalsIgnoreCase("unicast")){
 						client = words[1];
 						if(words[2].equalsIgnoreCase("file")){
 							message = words[3];
@@ -73,8 +74,12 @@ public class Client {
 						}
 						else{
 							message = messageSent.substring(words[0].length() + words[1].length() + 2);
-							System.out.println("Message is " + message);
+							//System.out.println("Message is " + message);
 						}
+					}
+					else{
+						System.out.println("You>  Incorrect message format!");
+						continue;
 					}
 					//System.out.println(file);
 
@@ -95,7 +100,7 @@ public class Client {
 
 		private void sendMessage(String mode, String client, String message) {
 			// TODO Auto-generated method stub
-			System.out.println("sending message\n" + mode + " " + client + " " + message);
+			System.out.println("You>   " + message);
 			ps.println(mode.toLowerCase());
 			ps.println(client.toLowerCase());
 			ps.println(message);
@@ -104,11 +109,10 @@ public class Client {
 
 		private void sendFile(String mode, String client, String message) throws IOException {
 			// TODO Auto-generated method stub
-			System.out.println("Sending file...");
 			//ObjectOutputStream out = new ObjectOutputStream(sock.getOutputStream());
 			ps.println(mode.toLowerCase() + " file");
 			ps.println(client.toLowerCase());
-			System.out.println(mode + " " + client + " " + message);
+			//System.out.println(mode + " " + client + " " + message);
 			File transferFile = new File ("C:\\Users\\Pratyoush\\Desktop\\clientserver\\client1\\"+message);
 			ps.println(transferFile.getName());
 			ps.println(transferFile.length());
@@ -116,14 +120,19 @@ public class Client {
 		    byte [] byteArray  = new byte [1024];
 		    //FileInputStream fin = new FileInputStream(transferFile);
 		    int c = 0;
-		    FileInputStream bis = (new FileInputStream(transferFile));
-		 		   		    
-		    while( (c = bis.read(byteArray, 0, byteArray.length)) != -1){
-	    	//ps.print(byteArray);
-	    	os.write(byteArray, 0, c);
+		    try{
+		    	FileInputStream bis = (new FileInputStream(transferFile));
+		    	while( (c = bis.read(byteArray, 0, byteArray.length)) != -1){
+		    		//ps.print(byteArray);
+		    		os.write(byteArray, 0, c);
+		    	}
+		    	os.flush();
+		    	bis.close();
+				System.out.println("You>   Sent a file: " + message);
 		    }
-		    os.flush();
-		    bis.close();		
+		    catch(FileNotFoundException e){
+		    	System.out.println("File not found. Please check the filename and retry.");
+		    }	
 		}
 	}
 	
@@ -142,9 +151,9 @@ public class Client {
 				br = new BufferedReader(ir);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				System.out.println(e1.getMessage());
 			}
-			System.out.println("Ready to receive");
+			//System.out.println("Ready to receive");
 			String messageRecv;
 			String sender;
 			while(true){
@@ -183,15 +192,14 @@ public class Client {
 							System.out.println(messageRecv);
 						}
 					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				} catch (FileNotFoundException e2){
+					System.out.println("File not found.");
 				}
-				
-			}		
-			
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					System.out.println(e.getMessage());
+				}	
+			}				
 		}
-		
 	}
-	
 }
--- src/clientServer/Server.java
+++ src/clientServer/Server.java
@@ -1,21 +1,16 @@
 package clientServer;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
